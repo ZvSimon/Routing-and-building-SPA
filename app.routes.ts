0ef8397fab5b0d816f92814168ef073be7dbf773
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-04 20:06:12 +0200

    Enabling Routing & Adding a First routes

---
+++ app.routes.ts
@@ -0,0 +1,9 @@
+import {TasksComponent} from "./src/app/tasks/tasks.component";
+import {Routes} from "@angular/router";
+
+export const routes:Routes = [
+    {
+        path: '/tasks',
+        component: TasksComponent
+    }
+]
