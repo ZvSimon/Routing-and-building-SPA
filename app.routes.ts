9855ad6bfa1c2046b0fa0a8342f21eedc4ff2397
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-04 20:22:08 +0200

    registering multiple routes

--- app.routes.ts
+++ app.routes.ts
@@ -1,9 +1,14 @@
 import {TasksComponent} from "./src/app/tasks/tasks.component";
 import {Routes} from "@angular/router";
+import {NoTaskComponent} from "./src/app/tasks/no-task/no-task.component";
 
 export const routes:Routes = [
     {
-        path: '/tasks',
+        path: '',
+        component:NoTaskComponent
+    },
+    {
+        path: 'tasks',
         component: TasksComponent
     }
 ]
